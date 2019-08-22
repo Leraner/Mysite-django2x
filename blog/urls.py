66f7d73ca90c92c2aaae15bb9b0d20344ed4b8ad
Learner <legend.tony@yandex.ru>
2019-08-22 12:34:49 +0300

    Add add post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListViews, BlogDetailView
+from .views import BlogListViews, BlogDetailView, BlockCreateView
 from home.views import AboutPageView
 
 
 urlpatterns = [
     path('', BlogListViews.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('about/', AboutPageView.as_view(), name='about')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('post/new/', BlockCreateView.as_view(), name='post_new')
 ]
